c9db68e814fb058fe45eaf5affa7a92937ac5e62
chathuraHarshanga <chathura749@gmail.com>
2024-06-18 11:15:12 +0530

    sse component

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/events', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  setInterval(() => {
+    const data = { message: 'Hello from server!', timestamp: new Date() };
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+  }, 3000);
+
+  req.on('close', () => {
+    console.log('Connection closed');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
